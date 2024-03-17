187a82d42ce506708cd325a772b5a6edddb74373
Maique <janofstargard@gmail.com>
2024-03-17 02:58:39 -0700

    fine-tune accelerometer inputs

--- app/src/zenCapeControls.c
+++ app/src/zenCapeControls.c
@@ -13,12 +13,10 @@
 #define ACCELEROMETER_HIHAT_DEBOUNCE_TIME 100
 #define ACCELEROMETER_SNARE_DEBOUNCE_TIME 100
 
-#define X_THRESHOLD 8000
-#define X_REBOUND 6000
-#define Y_THRESHOLD 8000
-#define Y_REBOUND 8000
-#define Z_THRESHOLD 8000
-#define Z_REBOUND 6000
+// 1 G is around 16000
+#define X_G_DIVISOR 12000 //decrease this value to make Hihat more sensitive
+#define Y_G_DIVISOR 14000 //decrease this value to make Snare more sensitive
+#define Z_G_DIVISOR 16000 //decrease this value to make Bass more sensitive
 
 static void* joystickInputThread();
 static void* accelerometerSamplingThread();
@@ -89,60 +87,47 @@ static void* accelerometerSamplingThread()
     // X normal range = {176, 352}
     // Y normal range = {-384, -32}
     // Z normal range = {16464, 16672}
+    // 1 G is about 16000 register val
     long long debounceTimerBass = getTimeInMs();
     long long debounceTimerHiHat = debounceTimerBass;
     long long debounceTimerSnare = debounceTimerBass;
     unsigned char* accInitial = accelerometer_readOutVals();
-    int16_t x_last = (accInitial[1] << 8) | accInitial[0];
-    int16_t y_last = (accInitial[3] << 8) | accInitial[2];
-    int16_t z_last = (accInitial[5] << 8) | accInitial[4];
-    int16_t x_floor = x_last - X_THRESHOLD;
-    int16_t y_floor = y_last - Y_THRESHOLD;
-    int16_t z_floor = z_last - Z_THRESHOLD;
-    int16_t min = z_last;
-    int16_t max = z_last;
+    int16_t x_last = ((accInitial[1] << 8) | accInitial[0]) / X_G_DIVISOR;
+    int16_t y_last = ((accInitial[3] << 8) | accInitial[2]) / Y_G_DIVISOR;
+    int16_t z_last = ((accInitial[5] << 8) | accInitial[4]) / Z_G_DIVISOR;
     free(accInitial);
     while(isRunning){
         unsigned char* accelerometerOutput = accelerometer_readOutVals();
-        int16_t x_data = (accelerometerOutput[1] << 8) | accelerometerOutput[0];
-        int16_t y_data = (accelerometerOutput[3] << 8) | accelerometerOutput[2];
-        int16_t z_data = (accelerometerOutput[5] << 8) | accelerometerOutput[4];
+        int16_t x_data_p = ((accelerometerOutput[1] << 8) | accelerometerOutput[0]);
+        int16_t y_data_p = ((accelerometerOutput[3] << 8) | accelerometerOutput[2]);
+        int16_t z_data_p = ((accelerometerOutput[5] << 8) | accelerometerOutput[4]);
+        int16_t x_data = x_data_p / X_G_DIVISOR;
+        int16_t y_data = y_data_p / Y_G_DIVISOR;
+        int16_t z_data = z_data_p / Z_G_DIVISOR;
         
         if ((getTimeInMs() - debounceTimerHiHat > ACCELEROMETER_HIHAT_DEBOUNCE_TIME) 
-            && x_last < x_floor 
-            && x_data > x_last + X_REBOUND){
-            // printf("HIHAT\n");
+            && x_last < 0 
+            && x_data >= 0){
             drumBeat_playHiHat();
             debounceTimerHiHat = getTimeInMs();
         }
         if ((getTimeInMs() - debounceTimerSnare > ACCELEROMETER_SNARE_DEBOUNCE_TIME) 
-            && y_last < y_floor 
-            && y_data > y_last + Y_REBOUND){
-            // printf("SNARE\n");
+            && y_last < 0 
+            && y_data >= 0){
             drumBeat_playHardSnare();
             debounceTimerSnare = getTimeInMs();
         }
         if ((getTimeInMs() - debounceTimerBass > ACCELEROMETER_BASS_DEBOUNCE_TIME) 
-            && z_last < z_floor 
-            && z_data > z_last + Z_REBOUND){
-            // printf("BASS\n");
+            && z_last > 1 
+            && z_data <= 1){
             drumBeat_playBass();
             debounceTimerBass = getTimeInMs();
         }
         x_last = x_data;
         y_last = y_data;
         z_last = z_data;
-        // printf("OUT_X_L val: %d\n", z_data);
         free(accelerometerOutput);
-        if (z_data < min){
-            min = z_data;
-        }
-        if (z_data > max){
-            max = z_data;
-        }
-        // printf("{%d, %d}\n", min, max);
-        // printf("%d\n", z_data);
-        
+        // printf("%d - {%d / %d}\n", z_data, z_data_p, Z_G_DIVISOR);
         sleepForMs(10);
     }
     pthread_exit(NULL);
